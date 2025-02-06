05d66a7ef2a657b2bf1955ee10e74dae1aeaf794
SÜMEYYE KORKMAZ <72557973+SUMEYYEKORKMAZ@users.noreply.github.com>
2025-02-06 16:38:05 +0300

    Add files via upload

---
+++ Bank_Account_Management.py
@@ -0,0 +1,74 @@
+'''
+Pair-2
+Sümeyye Korkmaz
+Fatma Büşra Tilki
+Ayşenur Göze
+Aslı Çoşkun
+
+'''
+
+class Account:
+    def __init__(self,account_holder,account_number,balance=0):
+        self.account_holder = account_holder
+        self.account_number = account_number
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+    
+    def set_balance(self,new_balance):
+        self.__balance=new_balance
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"{amount} TL deposited. Current balance : {self.__balance} TL")
+
+        else:
+            print("Invalid amount")
+
+    def withdraw(self,amount):
+        if 0 < amount <=self.__balance:
+            self.__balance -= amount
+            print(f"{amount} TL withdraw. Current balance :{self.__balance} TL")
+
+        else:
+            print("Insufficient balance or invalid amount")
+
+    def show_balance(self):
+        print(f"Account  Holder : {self.account_holder} , Balance : {self.__balance} TL")
+
+
+
+class CheckingAccount(Account):
+    pass 
+
+
+class SavingAccount(Account):
+    def __init__ (self, account_holder,account_number, balance, interest_rate):
+        super().__init__(account_holder, account_number, balance)
+        self.interest_rate = interest_rate
+
+    def calculate_interest (self):
+        current_balance = self.get_balance()
+        interest_amount=current_balance * self.interest_rate /100
+        print(f" Interest added to account : {interest_amount} TL")
+        self.set_balance(current_balance + interest_amount)
+
+    def withdraw(self, amount):
+        print("Withdrawals from saving account are limited.")
+        super().withdraw(amount)
+
+
+
+account1 = CheckingAccount ("Sümeyye Korkmaz", "0001", 1000)
+account1.show_balance()
+account1.deposit(500)
+account1.withdraw(400)
+
+
+account2 = SavingAccount("Ali Kaya", "0002", 2000, 5)
+account2.show_balance()
+account2.calculate_interest()
+account2.show_balance()
+account2.withdraw(500)
---
+++ Library_Management_System.py
@@ -0,0 +1,73 @@
+'''
+Pair-2
+Sümeyye Korkmaz
+Fatma Büşra Tilki
+Ayşenur Göze
+Aslı Çoşkun
+
+'''
+
+class Book:
+    def __init__(self, title, author, page_count, isbn):
+        self.__title = title
+        self.__author = author
+        self.__page_count = page_count
+        self.__isbn = isbn
+
+    def get_isbn(self):
+        return self.__isbn
+    
+    def __str__(self):
+        str = f"Book : {self.__title}, Author : {self.__author}, PAge Count : {self.__page_count}, ISBN : {self.__isbn}"
+        return str
+    
+
+class BookAlreadyExistsError(Exception):
+    pass
+
+
+class Library:
+
+    def __init__(self):
+        self.__books = []
+    
+    def add_book(self,book):
+        for b in self.__books:
+            if b.get_isbn() == book.get_isbn():
+                raise BookAlreadyExistsError(f"Book with ISBN {book.get_isbn()} already exists")
+        self.__books.append(book)
+        print(f"Book with ISBN {book.get_isbn()} succesfully added.")
+
+    def remove_book(self,isbn):
+        for book in self.__books:
+            if book.get_isbn()==isbn:
+                self.__books.remove(book)
+                print(f"Book with ISBN {isbn} succesfully removed.")
+                return
+        print(f"Book with ISBN {isbn} not found.")
+
+    def show_books(self):
+        if not self.__books:
+            print ("Library is empty.")
+        else:
+            for book in self.__books:
+                print(book)
+
+
+library = Library()
+
+books =[ 
+        Book("İnsancıklar", "Fyodor Dostoyevski", 175, "0001"),
+        Book("Kendime Düşünceler", "Marcus Aurelius", 184, "0002"),
+        Book("Beyaz Zambaklar Ülkesinde", "Grigory Petrow", 140, "0002"),
+        Book("Dorian Gray'in Potresi", "Oscar Wilde", 280, "0004")
+]
+
+for book in books:
+        try:
+            library.add_book(book)
+        except BookAlreadyExistsError as e:
+            print("Error:", e)
+
+
+library.show_books()
